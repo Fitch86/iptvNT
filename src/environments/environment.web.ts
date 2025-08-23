00d73ea19ff1f4ecbf688781ab291dd4f96d72d8
Fitch86 <128111344+Fitch86@users.noreply.github.com>
2025-08-23 17:44:37 +0800

    Update environment.web.ts

--- src/environments/environment.web.ts
+++ src/environments/environment.web.ts
@@ -2,5 +2,5 @@ export const AppConfig = {
     production: false,
     environment: 'WEB',
     version: require('../../package.json').version,
-    BACKEND_URL: 'https://iptvnator-playlist-parser-api.vercel.app',
+    BACKEND_URL: 'https://cors-proxy-three-pi.vercel.appp',
 };
