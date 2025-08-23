1f7c6ae5dc91ec4a2b6b14485a3e2c42f8af0a16
Fitch86 <128111344+Fitch86@users.noreply.github.com>
2025-08-23 15:52:35 +0800

    Update environment.web.ts

--- src/environments/environment.web.ts
+++ src/environments/environment.web.ts
@@ -2,5 +2,5 @@ export const AppConfig = {
     production: false,
     environment: 'WEB',
     version: require('../../package.json').version,
-    BACKEND_URL: 'https://iptvnator.vercel.app',
+    BACKEND_URL: 'https://iptvnator-playlist-parser-api.vercel.app',
 };
