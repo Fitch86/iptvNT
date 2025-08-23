38bd3f9cb47d813d4bb304ddf8318dbf07ca49ba
Fitch86 <128111344+Fitch86@users.noreply.github.com>
2025-08-23 15:46:40 +0800

    Update environment.web.ts

--- src/environments/environment.web.ts
+++ src/environments/environment.web.ts
@@ -2,5 +2,5 @@ export const AppConfig = {
     production: false,
     environment: 'WEB',
     version: require('../../package.json').version,
-    BACKEND_URL: 'https://iptvnt.netlify.app',
+    BACKEND_URL: 'https://iptvnator.vercel.app',
 };
