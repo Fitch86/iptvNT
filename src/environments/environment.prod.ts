52786d74c06ad8623d9d96c6776ad9f70ba61369
Fitch86 <128111344+Fitch86@users.noreply.github.com>
2025-08-24 10:59:54 +0800

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const AppConfig = {
     production: true,
     environment: 'PROD',
     version: require('../../package.json').version,
-    BACKEND_URL: 'https://iptv-cors.netlify.app',
+    BACKEND_URL: 'https://cors-proxy.judy.dpdns.org',
 };
